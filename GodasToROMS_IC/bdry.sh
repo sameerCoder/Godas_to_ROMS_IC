5a4db201803a210c4dbbcacc64682a2c4c3c759e
sameerCoder <39183783+sameerCoder@users.noreply.github.com>
2021-06-15 12:06:51 +0530

    Delete GodasToROMS_IC directory

--- GodasToROMS_IC/bdry.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-cd /home/sakib/Desktop/own_download_pyroms/pyroms/examples/Arctic_HYCOM_GLBy/bdry
-rm -f Godas_day*.nc
-cd ..
-python3 cppeditbdry.py 2019
-cd /home/sakib/Desktop/own_download_pyroms/pyroms/examples/Arctic_HYCOM_GLBy/bdry
-ncrcat Godas_day*.nc final_Godas_bdry.nc
